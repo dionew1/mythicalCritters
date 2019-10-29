fa5edc755cc30d563e18b9f7a3ef725781366d8c
Dione Wilson <dionew1@gmail.com>
2019-10-29 15:30:23 -0600

    Complete Unicorn

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,5 +1,13 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name
+    this.color = color
+  }
+  isWhite(){
+    return this.color === 'white'
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
